24212cee703b7968c0972cc876181d31578380e4
BlissTerra <iuliano2k@gmail.com>
2015-03-21 22:17:42 -0700

    Added JavaScript task to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,42 @@
+// ##################################################################################################################################
+// Author: Iacob Silviu - Iulian
+// Project: WorkflowWithGulp
+// Date: 3 / 21 / 2015
+// ##################################################################################################################################
+// Nodejs Requires 
+// ##################################################################################################################################
+
 var gulp 		= require('gulp'),
 	gutil 		= require('gulp-util'),
-	coffee		= require('gulp-coffee');
+	coffee		= require('gulp-coffee'),
+	concat		= require('gulp-concat');
 
-var coffeeSources = ['components/coffee/tagline.coffee'];
+// ##################################################################################################################################
+// File locations
+// ##################################################################################################################################
+
+var coffeeSources = ['components/coffee/tagline.coffee'],
+    jsSources	  = ['components/scripts/rclick.js', 
+					 'components/scripts/pixgrid.js',
+				 	 'components/scripts/tagline.js',
+				 	 'components/scripts/template.js'];
+
+// ##################################################################################################################################
+// CoffeeScript Task
+// ##################################################################################################################################
 
 gulp.task('coffee', function() {
 	gulp.src(coffeeSources)
 		.pipe(coffee({ bare: true }).on('error', gutil.log))	//compiles to js w/o putting it into a secured wrapper.
 		.pipe(gulp.dest('components/scripts'));
+});
+
+// ##################################################################################################################################
+// JavaScript Task
+// ##################################################################################################################################
+
+gulp.task('js', function() {
+	gulp.src(jsSources)
+		.pipe(concat('script.js'))
+		.pipe(gulp.dest('builds/development/js'));
 });
