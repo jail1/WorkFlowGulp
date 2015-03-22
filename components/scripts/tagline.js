40aa13a8197aaf79d9854e19b814af5ea978387c
BlissTerra <iuliano2k@gmail.com>
2015-03-21 22:06:46 -0700

    Added CoffeeScript task to Gulp.

---
+++ components/scripts/tagline.js
@@ -0,0 +1,5 @@
+var fill;
+
+(fill = function(item) {
+  return $('.tagline').append('#{item}');
+})('The most cretive minds in the Art');
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 var gulp 		= require('gulp'),
-	gutil 		= require('gulp-util');
+	gutil 		= require('gulp-util'),
+	coffee		= require('gulp-coffee');
 
-gulp.task('log', function() {
-	gutil.beep();
+var coffeeSources = ['components/coffee/tagline.coffee'];
+
+gulp.task('coffee', function() {
+	gulp.src(coffeeSources)
+		.pipe(coffee({ bare: true }).on('error', gutil.log))	//compiles to js w/o putting it into a secured wrapper.
+		.pipe(gulp.dest('components/scripts'));
 });
